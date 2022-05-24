2c575a906ffa8e3c86203d3d55076688b6871806
kristijan_00 <kristijandjakovic10@gmail.com>
2022-05-24 16:30:44 +0200

    Minor changes

--- js/main.js
+++ js/main.js
@@ -6,10 +6,11 @@
 const toggleBtn = document.querySelector('#toggleBtn');
 const divList = document.querySelector('.listHolder');
 const listUl = document.querySelector('.list');
+const body = document.querySelector('body');
 const lis = listUl.children;
 var map = new Map()
 const logOutBtn = document.querySelector('#logOutBtn');
-var email; 
+var email;
 
 logOutBtn.addEventListener('click', () => {
   signOut();
@@ -17,9 +18,7 @@ logOutBtn.addEventListener('click', () => {
 
 // check if the user is logged in
 window.onload = function () {
-   //const url = "https://murmuring-refuge-03345.herokuapp.com/login";
   const url = "https://murmuring-refuge-03345.herokuapp.com/home";
-  //const url = "http://127.0.0.1:3000/login"; 
 
   var client = new XMLHttpRequest();
 
@@ -30,9 +29,10 @@ window.onload = function () {
   if (client.status == 200) {
 
     if (client.responseText === "0") {
-      // alert("Wrong credentials!");
+      alert("Login to view this page!");
+      window.location.replace("http://127.0.0.1:5500/html/login.html");
     } else {
-      alert("Logged in: " + client.responseText);
+      body.style.display = 'block';
       logOutBtn.style.display = 'block';
     }
 
@@ -41,7 +41,7 @@ window.onload = function () {
 
 //sign out the user from the current session
 function signOut() {
-   const url = "https://murmuring-refuge-03345.herokuapp.com/logout";
+  const url = "https://murmuring-refuge-03345.herokuapp.com/logout";
 
   var client = new XMLHttpRequest();
 
@@ -62,7 +62,7 @@ function signOut() {
 };
 
 
-viewList(); 
+viewList();
 // action to be taken when clicked on hide list button
 toggleBtn.addEventListener('click', () => {
   if (divList.style.display === 'none') {
@@ -95,77 +95,77 @@ function addLists() {
     let title = addInput.value;
     console.log(title)
 
-    email = localStorage.getItem("email"); 
+    email = localStorage.getItem("email");
 
-    console.log(email); 
+    console.log(email);
     let jsonData = JSON.stringify({
       'title': title, 'email': email
     });
 
-   
-     const url = "https://murmuring-refuge-03345.herokuapp.com/saveNote";
+
+    const url = "https://murmuring-refuge-03345.herokuapp.com/saveNote";
     // const url = "http://127.0.0.1:3000/saveNote"; 
-     var client = new XMLHttpRequest();
-     
-     client.open("POST", url, false);
-     client.setRequestHeader("Content-Type", "application/json");
-     client.send(jsonData);
-     
-      if (client.status == 200){
-        // alert("The request succeeded!\n\nThe response representation was:\n\n" + client.responseText)
-        li.innerHTML = title;
-        ul.appendChild(li);
-        createBtn(li);
+    var client = new XMLHttpRequest();
+
+    client.open("POST", url, false);
+    client.setRequestHeader("Content-Type", "application/json");
+    client.send(jsonData);
+
+    if (client.status == 200) {
+      // alert("The request succeeded!\n\nThe response representation was:\n\n" + client.responseText)
+      li.innerHTML = title;
+      ul.appendChild(li);
+      createBtn(li);
       $("#addInput").val("");
       window.location.reload();
 
-      
-       }
+
+    }
 
   }
 }
 function viewList() {
-  email = localStorage.getItem("email"); 
-  console.log(localStorage.getItem("email")); 
+  email = localStorage.getItem("email");
+  console.log(localStorage.getItem("email"));
 
   const url = "https://murmuring-refuge-03345.herokuapp.com/notebyEmail";
-// const url = "http://127.0.0.1:3000/notebyEmail";
+  // const url = "http://127.0.0.1:3000/notebyEmail";
 
   let jsonData = JSON.stringify({
     'email': email
   });
 
-     var client = new XMLHttpRequest();
-     
-     client.open("POST", url, false);
-     client.setRequestHeader("Content-Type", "application/json");
-     client.send(jsonData);
-      if (client.status == 200){
-        // alert("The request succeeded!\n\nThe response representation was:\n\n" + client.responseText)
-        let notes = JSON.parse(client.response);
-        var allNotes = notes.results.map(d => d.title);
-        var getTheId = notes.results.map(d => d.noteid);
-        console.log(allNotes); 
-        for (let index = 0; index < allNotes.length; index++) {
-          const ul = divList.querySelector('ul');
-          const li = document.createElement('li');
-          const noteId = getTheId[index];
-          const getNotes = allNotes[index]
-          li.innerHTML = getNotes; 
-          li.setAttribute("id", noteId); 
-          ul.appendChild(li);
-          saveNoteId(li, noteId); 
-          map.set(noteId, li)
-          console.log("Note " + allNotes[index] + ": ID: " + getTheId[index]); 
-        }
-     
+  var client = new XMLHttpRequest();
+
+  client.open("POST", url, false);
+  client.setRequestHeader("Content-Type", "application/json");
+  client.send(jsonData);
+  if (client.status == 200) {
+    // alert("The request succeeded!\n\nThe response representation was:\n\n" + client.responseText)
+    let notes = JSON.parse(client.response);
+    var allNotes = notes.results.map(d => d.title);
+    var getTheId = notes.results.map(d => d.noteid);
+    console.log(allNotes);
+    for (let index = 0; index < allNotes.length; index++) {
+      const ul = divList.querySelector('ul');
+      const li = document.createElement('li');
+      const noteId = getTheId[index];
+      const getNotes = allNotes[index]
+      li.innerHTML = getNotes;
+      li.setAttribute("id", noteId);
+      ul.appendChild(li);
+      saveNoteId(li, noteId);
+      map.set(noteId, li)
+      console.log("Note " + allNotes[index] + ": ID: " + getTheId[index]);
+    }
+
   }
-     
-     
+
+
 }
 
 function deleteList() {
-  
+
 }
 
 
@@ -195,21 +195,21 @@ function createBtn(li) {
   return li;
 }
 
-function saveNoteId (li, noteId) { 
+function saveNoteId(li, noteId) {
   const id = document.createElement('Label');
   id.className = 'getId';
-//$('.getId').attr("hidden", false);
- id.id = noteId; 
- li.appendChild(id); 
- return li; 
- }
+  //$('.getId').attr("hidden", false);
+  id.id = noteId;
+  li.appendChild(id);
+  return li;
+}
 
- 
+
 
 
 for (var i = 0; i < lis.length; i++) {
   createBtn(lis[i]);
-  }
+}
 
 /* 
 4. enabling button actions (to move item up, down or delete)
@@ -218,33 +218,33 @@ for (var i = 0; i < lis.length; i++) {
 divList.addEventListener('click', (event) => {
   if (event.target.tagName === 'BUTTON') {
     const button = event.target;
-   const li = button.parentNode;
+    const li = button.parentNode;
     const ul = li.parentNode;
     if (button.className === 'btn-icon remove') {
-      var id = $(li).attr("id"); 
+      var id = $(li).attr("id");
       let jsonData = JSON.stringify({
         'id': id,
       });
-       const url = "https://murmuring-refuge-03345.herokuapp.com/delete";
+      const url = "https://murmuring-refuge-03345.herokuapp.com/delete";
       // const url = "http://127.0.0.1:3000/delete";
-       
 
-       var client = new XMLHttpRequest();
-       
-       client.open("POST", url, false);
-       client.setRequestHeader("Content-Type", "application/json");
-       client.send(jsonData);
 
-        if (client.status == 200){
-          // alert("The request succeeded!\n\nThe response representation was:\n\n" + client.responseText)
-           if(client.response==1){
-            ul.removeChild(li);
-            console.log("Deleted"); 
+      var client = new XMLHttpRequest();
 
-           
-    }
+      client.open("POST", url, false);
+      client.setRequestHeader("Content-Type", "application/json");
+      client.send(jsonData);
+
+      if (client.status == 200) {
+        // alert("The request succeeded!\n\nThe response representation was:\n\n" + client.responseText)
+        if (client.response == 1) {
+          ul.removeChild(li);
+          console.log("Deleted");
+
+
         }
-       
-    } 
+      }
+
+    }
   }
 });
